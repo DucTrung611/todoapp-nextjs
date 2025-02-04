11bb7e9caa84729238994ff3cb353f39e0810daa
DucTrung611 <thanductrung123@gmail.com>
2025-02-04 11:16:58 +0700

    update taidwind css

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -16,27 +16,32 @@ const TodoApp = () => {
     };
     return (
         <div className="">
-            <div className="text-center container mx-auto bg-rose-50 h-screen border-spacing-1">
+            <div className="w-[500px] mx-auto p-4 text-center">
                 <h2 className="py-10 font-medium text-2xl m-2">To-Do App</h2>
-                <div className="pb-4">
-                    <input
-                        type="text"
-                        value={task}
-                        onChange={(e) => setTask(e.target.value)}
-                        placeholder="Add a new task"
-                        className="bg-gray-50 border border-gray-300"
-                    />
-                    <button className='ml-20 border border-gray-300' onClick={addTask}>Add</button>
+                <div className="items-center w-30">
+                    <div className="flex gap-3 pb-4">
+                        <input
+                            type="text"
+                            value={task}
+                            onChange={(e) => setTask(e.target.value)}
+                            placeholder="Add a new task"
+                            className="border text-sm rounded-lg focus:ring-blue focus:border-blue block w-full px-4 py-2"
+                        />
+                        <button className='bg-white border  focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5   content-center bg-orange flex items-center' onClick={addTask}>Add</button>
+                    </div>
+
+                    <ul>
+                        {tasks.map((value, index) => (
+                            <li className="flex justify-between w-full p-4" key={index}>
+                                <div className=" flex items-center">
+                                    {value}
+                                </div>
+                                <button className=' bg-white border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 content-center bg-orange' onClick={() => removeTask(index)}>Remove</button>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
 
-                <ul>
-                    {tasks.map((value, index) => (
-                        <li className="pb-3" key={index}>
-                            {value}
-                            <button className='pl-20' onClick={() => removeTask(index)}>Remove</button>
-                        </li>
-                    ))}
-                </ul>
             </div >
         </div >
     )
